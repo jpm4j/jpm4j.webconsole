37d8a89fcf29c0e483ab50fac513d71df195b158
Peter Kriens <peter.kriens@aqute.biz>
2013-02-12 19:09:57 +0100

    Refresh the window when we do an install

--- jpm4j.webconsole/src/jpm4j/webconsole/impl/js/plugins.js
+++ jpm4j.webconsole/src/jpm4j/webconsole/impl/js/plugins.js
@@ -40,6 +40,8 @@
 					$scope.programs = data.programs || $scope.programs; 
 					$scope.programs = data.programs || $scope.programs; 
 					$scope.main.alert= data.error;
+					if ( action === 'INSTALL')
+						document.location.reload(true);
 					
 				} )
 				.error( function(data,status) {
